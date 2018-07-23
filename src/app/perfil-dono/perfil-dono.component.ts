44b85414b137c28df3c444d3d0fc71abd427a356
Lucas Brito <lucasbsilva29@gmail.com>
2018-07-23 16:44:12 -0300

    Finalização do perfil do dono

--- src/app/perfil-dono/perfil-dono.component.ts
+++ src/app/perfil-dono/perfil-dono.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil-dono.component.css']
 })
 export class PerfilDonoComponent implements OnInit {
+  DonoNome = "Lucas de Brito Silva";
+  DonoDtaNasc = "27/04/1999";
+  DonoEmail = "lucasbsilva29@gmail.com";
+  DonoPass = "*****123";
+  PetNome = "Bjorn";
 
   constructor() { }
 
